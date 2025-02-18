f018a38f007885d78a96b9da62518506412cd0ee
KunRalf <rshumaher1996@gmail.com>
2025-02-19 00:05:12 +0300

    add yandex plugin

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public class PauseMenu : MonoBehaviour
+    {
+        private bool isPaused = false;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Tab))
+            {
+                TogglePause();
+            }
+        }
+
+        private void TogglePause()
+        {
+            isPaused = !isPaused;
+
+            if (isPaused)
+            {
+          
+                Time.timeScale = 0f;
+               
+                AudioListener.pause = true;
+            }
+            else
+            {
+                
+                Time.timeScale = 1f;
+              
+                AudioListener.pause = false;
+            }
+        }
+    }
+}
